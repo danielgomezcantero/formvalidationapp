b66846815661b50525fc7268d066380be3510670
Daniel Maximiliano Gómez Cantero <danielgomezcantero@gmail.com>
2020-09-15 07:54:05 -0400

    Estructura de proyecto

---
+++ lib/src/pages/login_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        _creaFondo(context),
+      ],
+    ));
+  }
+
+  Widget _creaFondo(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final fondoMorado = Container(
+      height: size.height * 0.4,
+      width: double.infinity,
+      decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+        Color.fromRGBO(63, 63, 156, 1.0),
+        Color.fromRGBO(90, 70, 178, 1.0),
+      ])),
+    );
+
+    final circulo = Container(
+      width: 100.0,
+      height: 100.0,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100.0),
+          color: Color.fromRGBO(255, 255, 255, 0.08)),
+    );
+
+    return Stack(
+      children: [fondoMorado, circulo],
+    );
+  }
+}
